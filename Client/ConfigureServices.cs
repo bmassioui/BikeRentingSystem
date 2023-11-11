d79de09c147f5734b8dd27b54e529898d974e292
bmassioui <bouchaib.massioui@gmail.com>
2023-11-11 17:50:54 +0000

    :construction: Define Client specification using NSwag

---
+++ Client/ConfigureServices.cs
@@ -0,0 +1,18 @@
+﻿using BikeRentalSystem.Client.Services.Client.Generated;
+using System.Net.Http.Headers;
+
+namespace BikeRentalSystem.Client;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddBikeRentalSystemClientServices(this IServiceCollection services)
+    {
+        services.AddHttpClient<IBikesClient, BikesClient>((serviceProvider, httpClient) =>
+        {
+            httpClient.BaseAddress = new Uri("https://localhost:7092/api/");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        });
+
+        return services;
+    }
+}
