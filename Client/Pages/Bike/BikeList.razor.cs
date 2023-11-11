39d70fe964cabd5eaf8e8b148495ec08290fc68f
bmassioui <bouchaib.massioui@gmail.com>
2023-11-11 23:44:30 +0000

    :fire: Code cleanup by removing WeatherForcast feature

--- Client/Pages/Bike/BikeList.razor.cs
+++ Client/Pages/Bike/BikeList.razor.cs
@@ -1,5 +1,18 @@
-﻿namespace BikeRentalSystem.Client.Pages.Bike;
+﻿using BikeRentalSystem.Client.Services.Client.Generated;
+using BikeRentalSystem.Shared.Bike;
+using Microsoft.AspNetCore.Components;
+
+namespace BikeRentalSystem.Client.Pages.Bike;
 
 public partial class BikeList
 {
+    [Inject]
+    public IBikesClient BikesClient { get; set; } = default!;
+
+    public IReadOnlyList<BikeDto> Bikes { get; set; } = Enumerable.Empty<BikeDto>().ToList();
+
+    protected async override Task OnInitializedAsync()
+    {
+        Bikes = (IReadOnlyList<BikeDto>)await BikesClient.BikesAsync();
+    }
 }
